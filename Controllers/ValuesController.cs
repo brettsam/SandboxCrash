edb0152aaa781d56fbb25afd6a27d88ba8308b6a
Brett Samblanet <brettsam@microsoft.com>
2018-05-22 15:41:57 -0700

    repro

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace SandboxCrash.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private static Lazy<CloudBlobContainer> _containerLazy = new Lazy<CloudBlobContainer>(() =>
+        {
+            var account = CloudStorageAccount.Parse("PUT STORAGE CONNECTION STRING HERE");
+            var client = account.CreateCloudBlobClient();
+            var container = client.GetContainerReference("sandboxcrash");
+            container.CreateIfNotExistsAsync().GetAwaiter().GetResult();
+            return container;
+        });
+
+        private static CloudBlobContainer _container = _containerLazy.Value;
+
+        [HttpPost("{count}/{delay}")]
+        public async Task Post(int count, int delay)
+        {
+            var cts = new CancellationTokenSource();
+            var tasks = new List<Task>();
+
+            for (int i = 0; i < count; i++)
+            {
+                var blob = _container.GetBlockBlobReference(Guid.NewGuid().ToString());
+                tasks.Add(blob.UploadTextAsync(i.ToString(), null, null, null, null, cts.Token));
+            }
+
+            await Task.Delay(delay);
+            cts.Cancel();
+
+            try
+            {
+                await Task.WhenAll(tasks);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
